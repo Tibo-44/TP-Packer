13839a1f8a470ce1c5d213105e874de1c3aaa2fa
Thibaud Robin <thibaudrobin44@gmail.com>
2021-12-07 05:46:27 +0100

    TP-Packer

--- launch-vm.sh
+++ launch-vm.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-echo Installation de Go-Lang
+
+# Installation de golang-myip
+
+echo Installation de golang-myip
 yum install git golang -y
 git clone https://github.com/BastienBalaud/golang-myip.git
-cd /root/tp-1/golang-myip
+cd /root/golang-myip
+
+# Build du server.go
 go build server.go
+
+# Déplacement dans le répertoire des binaires
 cp server /usr/local/bin/
+
+# Création du service golang
 cd /etc/systemd/system/
 echo "
 [Unit]
-Description=Run myip as Service
-ConditionPathExists=/root/tp-1/golang-myip
+Description=Run golang as service
+ConditionPathExists=/root/golang-myip
 After=network.target
 
 [Service]
@@ -17,14 +26,28 @@ Type=simple
 User=root
 Group=root
 
-WorkingDirectory=/root/tp-1/golang-myip/
-ExecStart=/usr/local/bin/server
+WorkingDirectory=/root/golang-myip/
+# Rendre accessible goland sur le port 80
+ExecStart=/usr/local/bin/server --port 80
 Restart=on-failure
 RestartSec=10
 
+
 [Install]
 WantedBy=multi-user.target
-" > myip.service
-systemctl enable myip.service
-systemctl start myip.service
-echo "FIN"
+" > golang.service
+
+# Démarrage du service golang
+systemctl enable golang.service
+systemctl start golang.service
+
+# Ajout des clés SSH
+cd /root/.ssh
+echo"
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjIRqNf3oVs8vbAcoNHzXMxlS5fL6qqFxfdi221S9p6
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAmM7WeoXFKZGnDLV8RA3jfeYKPIYzTNIe3Yqr7Z11BV
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXNu2wW1ssMqlzRd3q/YodypM77UNFz7tF6h2v+hYYZ
+" >> authorized_keys
+
+echo "FIN SCRIPT LAUNCH-VM"
+sleep 200
